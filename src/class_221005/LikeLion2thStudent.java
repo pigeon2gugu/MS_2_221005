0f5ed29245e5b69c2614fb367e0e05bbad5e31a5
DESKTOP-892OP9V\Administrator <khn1135@gmail.com>
2022-10-05 16:01:44 +0900

    리스트에 이름 넣어서 출력하는 코드
    1. LikeLion2thStudent : 반, 이름, 깃허브주소 특성을 갖는 오브젝트
    2. LikeLion2thName : 이름 100개 추가하는 클래스 + LikeLion2thStudent에 정보 추가
    3. LikeLion2th : LikeLion2thName에서 추가된 정보를 불러오는 클래스
    4. LikeLion2thMain : psvm 메인 클래스. 리스트 출력 (+ 리스트 길이 출력)

---
+++ src/class_221005/LikeLion2thStudent.java
@@ -0,0 +1,17 @@
+package class_221005;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LikeLion2thStudent {
+
+    public int classNo;
+    public String name;
+    public String gitRepositoryAddress;
+
+    public LikeLion2thStudent(int classNo, String name, String gitRepositoryAddress) {
+        this.classNo = classNo;
+        this.name = name;
+        this.gitRepositoryAddress = gitRepositoryAddress;
+    }
+}

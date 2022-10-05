8dd3fef7a23e8432ebf2884dc7f3a5a0886bb638
DESKTOP-892OP9V\Administrator <khn1135@gmail.com>
2022-10-05 10:48:52 +0900

    Calculator 1. Calculator class file (생성자 생성) 2. Calculator Main 실행 file

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,18 +1,27 @@
 public class Calculator {
-    public void plus(int a, int b) {
+
+    private int a;
+    private int b;
+
+    public Calculator(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public void plus() {
         System.out.println(a + b);
     }
 
-    public void minus(int a, int b) {
+    public void minus() {
         System.out.println(a - b);
     }
 
-    public void multiple(int a, int b) {
+    public void multiple() {
         System.out.println(a * b);
     }
 
-    public void divide(int a, int b) {
-        float result = (float)a/(float)b;
+    public void divide() {
+        float result = (float)a/b;
         System.out.println(result);
     }
 
--- src/CalculatorMain.java
+++ src/CalculatorMain.java
@@ -2,15 +2,12 @@ public class CalculatorMain {
 
     public static void main(String[] args) {
 
-        Calculator calculator = new Calculator();
+        Calculator calculator = new Calculator(10, 20);
 
-        int a = 10;
-        int b = 20;
-
-        calculator.plus(a, b);
-        calculator.minus(a, b);
-        calculator.multiple(a, b);
-        calculator.divide(a, b);
+        calculator.plus();
+        calculator.minus();
+        calculator.multiple();
+        calculator.divide();
 
     }
 }

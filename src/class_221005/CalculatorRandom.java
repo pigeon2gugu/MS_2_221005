eb34d8f7073e0ffb5dc5cd707757282a159e4930
DESKTOP-892OP9V\Administrator <khn1135@gmail.com>
2022-10-05 11:15:01 +0900

    Random Number Calculator 지정 숫자 + 랜덤 넘버 계산기 1. Calculator class file 2. Calculator Main 실행 file

---
+++ src/class_221005/CalculatorRandom.java
@@ -0,0 +1,42 @@
+package class_221005;
+public class CalculatorRandom {
+
+    private int a;
+    private int b;
+
+    public int getRandomNum() {
+        return (int) (Math.random() * 10);
+    }
+
+    public CalculatorRandom(int a) {
+        this.a = a;
+        this.b = getRandomNum();
+    }
+
+
+
+    public void plus() {
+        System.out.println(a + b);
+    }
+
+    public void minus() {
+        System.out.println(a - b);
+    }
+
+    public void multiple() {
+        System.out.println(a * b);
+    }
+
+    public void divide() {
+
+        if(b == 0) {
+            System.out.println("랜덤하게 생성된 숫자가 0입니다.");
+        } else {
+            float result = (float)a/b;
+            System.out.println(result);
+        }
+
+    }
+
+}
+
---
+++ src/class_221005/CalculatorRandomMain.java
@@ -0,0 +1,16 @@
+package class_221005;
+public class CalculatorRandomMain {
+
+    public static void main(String[] args) {
+
+        CalculatorRandom calculator = new CalculatorRandom(10);
+
+        calculator.plus();
+        calculator.minus();
+        calculator.multiple();
+        calculator.divide();
+
+    }
+
+}
+

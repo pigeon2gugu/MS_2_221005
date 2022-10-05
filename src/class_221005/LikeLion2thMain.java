6188ad2743090104d9380f176bfad565143e83d9
DESKTOP-892OP9V\Administrator <khn1135@gmail.com>
2022-10-05 15:34:21 +0900

    리스트에 이름 넣어서 출력하는 코드
    1. LikeLion2thName : 이름 100개 추가하는 클래스
    2. LikeLion2th : 이름 추가된 리스트를 불러오는 클래스
    3. LikeLion2thMain : psvm 메인 클래스. 리스트 출력 (+ 리스트 길이 출력)

--- src/class_221005/LikeLion2thMain.java
+++ src/class_221005/LikeLion2thMain.java
@@ -7,8 +7,12 @@ public class LikeLion2thMain {
         LikeLion2th likeLion2th = new LikeLion2th();
         List<String> students = likeLion2th.getStudentList();
 
+        //for each
         for (String student : students) {
             System.out.println(student);
         }
+
+        //list에 들어있는 element 개수 세기
+        System.out.println(students.size());
     }
 }

1e54300c104a55cd7afd7b8dae6a253167aad425
DESKTOP-892OP9V\Administrator <khn1135@gmail.com>
2022-10-06 13:42:20 +0900

    Map Exercise 2_1 1. MapExercise2_1 : 알파벳인지 boolean으로 체크 후 개수 세기

--- src/class_221006/MapExercise2_1.java
+++ src/class_221006/MapExercise2_1.java
@@ -1,9 +1,11 @@
 package class_221006;
 
+import java.util.HashMap;
+
 public class MapExercise2_1 {
 
-    public static boolean isAlphabet(char alpha) {
-        if (((int)alpha >= 65&&(int)alpha >= 65+25 ) || ((int)alpha >= 97&&(int)alpha >= 97+25)) {
+    public boolean isAlphabet(char alpha) {
+        if ((alpha >= 'A' && alpha <='Z') || (alpha >= 'a' && alpha <= 'z')) {
             return true;
         } else {
             return false;
@@ -12,7 +14,26 @@ public class MapExercise2_1 {
 
     public static void main(String[] args) {
 
-        System.out.println(isAlphabet(';'));
+        MapExercise2_1 isAlpha = new MapExercise2_1();
+
+        String s1 = "aabbbbcdeefghhiijj".toUpperCase();
+        HashMap<Character, Integer> alpha = new HashMap<>();
+
+        for(char c = 'A'; c <'Z'; c++) {
+            alpha.put(c,0);
+        }
+
+        for (int i = 0; i < s1.length(); i++) {
+            char c = s1.charAt(i);
+            boolean isAlphabetB = isAlpha.isAlphabet(c);
+            if(isAlphabetB) {
+                alpha.put(c, alpha.get(c)+1);
+            }
+        }
+
+        System.out.println(alpha);
+
+
 
     }
 }

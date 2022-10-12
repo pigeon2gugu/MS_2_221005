6382276b9ff7f385bfb01c77a88d8b36e71040cf
pigeon2gugu <khn1135@gmail.com>
2022-10-12 17:29:34 +0900

    아기사자반 구구단 1. Gugudan : 구구단 출력 * 2. GugudanPlus : 구구단 출력 +

---
+++ src/class_221012/BabyLion/Gugudan.java
@@ -0,0 +1,14 @@
+package class_221012.BabyLion;
+import java.util.Scanner;
+
+public class Gugudan {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int input = sc.nextInt();
+
+        for(int i = 1; i<10; i++) {
+            System.out.println(input+"*"+i+"="+input*i);
+        }
+    }
+}

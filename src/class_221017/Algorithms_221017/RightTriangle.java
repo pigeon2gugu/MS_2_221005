980f5588ddc806d6d010ba27a395a715f84e24a7
pigeon2gugu <khn1135@gmail.com>
2022-10-17 09:07:03 +0900

    알고리즘 221017 1. 직각삼각형 별찍기. sout으로 단순 출력.

---
+++ src/class_221017/Algorithms_221017/RightTriangle.java
@@ -0,0 +1,11 @@
+package class_221017.Algorithms_221017;
+
+public class RightTriangle {
+
+    public static void main(String[] args) {
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+    }
+}

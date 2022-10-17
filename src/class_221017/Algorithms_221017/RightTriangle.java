a677e162f837a5da676d716d31325595dcd373b7
pigeon2gugu <khn1135@gmail.com>
2022-10-17 09:31:15 +0900

    알고리즘 221017 1. 직각삼각형 별찍기. 이중 포문이용 + 별 외에도 다른 문자 출력

--- src/class_221017/Algorithms_221017/RightTriangle.java
+++ src/class_221017/Algorithms_221017/RightTriangle.java
@@ -2,16 +2,21 @@ package class_221017.Algorithms_221017;
 
 public class RightTriangle {
 
+    private String letter;
+    public RightTriangle(String n) {
+        this.letter = n;
+    }
+
     void printStar(int n) {
         for(int i = 0; i <n; i++) {
             for(int j = 0; j <= i; j++) {
-                System.out.printf("*");
+                System.out.printf(this.letter);
             }
             System.out.println();
         }
     }
     public static void main(String[] args) {
-        RightTriangle rt = new RightTriangle();
-        rt.printStar(4);
+        RightTriangle rt = new RightTriangle("!");
+        rt.printStar(10);
     }
 }

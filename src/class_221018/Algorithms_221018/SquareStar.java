42cb8df30b4bda6934fd91edb5b0ee31a7aa46a3
pigeon2gugu <khn1135@gmail.com>
2022-10-18 09:12:04 +0900

    알고리즘 221018 1번째 1. 정사각형 별 찍기

---
+++ src/class_221018/Algorithms_221018/SquareStar.java
@@ -0,0 +1,26 @@
+package class_221018.Algorithms_221018;
+
+public class SquareStar {
+
+    private int length = 0;
+
+    public SquareStar(int n) {
+        this.length = n;
+    }
+
+    void printStar() {
+        for (int i = 0; i <this.length ; i++) {
+            for(int j = 0; j<this.length; j++){
+                System.out.printf("*");
+            }
+            System.out.println();
+        }
+
+    }
+
+
+    public static void main(String[] args) {
+        SquareStar ss = new SquareStar(5);
+        ss.printStar();
+    }
+}

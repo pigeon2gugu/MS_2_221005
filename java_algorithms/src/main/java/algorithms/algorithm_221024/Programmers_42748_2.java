bc07060c522cce94d68532d5f41611c64f61d2a6
pigeon2gugu <khn1135@gmail.com>
2022-10-24 10:17:38 +0900

    Programmers algorithm no.42748 ver 2 sort algorithm use priorityQueue

---
+++ java_algorithms/src/main/java/algorithms/algorithm_221024/Programmers_42748_2.java
@@ -0,0 +1,43 @@
+package algorithms.algorithm_221024;
+//https://school.programmers.co.kr/learn/courses/30/lessons/42748?language=java
+//정렬 알고리즘
+//priority queue 사용
+
+import java.util.PriorityQueue;
+
+public class Programmers_42748_2 {
+
+    public int getKthNum (int[] command, int[] arr) {
+        int frIdx = command[0]; //from index
+        int toInx = command[1]; //to index
+        int nth = command[2]; //pick index
+        int result = 0;
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for(int i = frIdx-1; i < toInx; i++)  {
+            pq.add(arr[i]);
+
+        }
+        System.out.println(pq);
+        for (int i = 0; i < nth; i++) {
+            result = pq.poll(); // stack의 pop과 비슷
+        }
+        return result;
+    }
+
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+
+        for (int i = 0; i < commands.length; i++) {
+            answer[i] = getKthNum(commands[i], array);
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Programmers_42748_2 pg = new Programmers_42748_2();
+        int[] arrT = {1, 5, 2, 6, 3, 7, 4};
+        int[][] comT = {{2, 5, 3}, {4, 4, 1}, {1, 7, 3}};
+        pg.solution(arrT, comT);
+    }
+}

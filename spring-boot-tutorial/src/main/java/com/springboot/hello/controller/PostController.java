4857d2b4087e81e62ebbf6cd8183196552c77629
pigeon2gugu <khn1135@gmail.com>
2022-10-27 14:38:25 +0900

    spring-boot tutorial 1. Apply ResponseEntity to PostController

--- spring-boot-tutorial/src/main/java/com/springboot/hello/controller/PostController.java
+++ spring-boot-tutorial/src/main/java/com/springboot/hello/controller/PostController.java
@@ -1,6 +1,8 @@
 package com.springboot.hello.controller;
 
 import com.springboot.hello.domain.dto.MemberDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
@@ -37,5 +39,13 @@ public class PostController {
     @PostMapping("/member2")
     public String postMemberDto(@RequestBody MemberDto memberDto) {
         return memberDto.toString();
+    }
+
+    @PostMapping("/member3")
+    public ResponseEntity<MemberDto>
+    putMember(@RequestBody MemberDto memberDto) {
+        return ResponseEntity
+                .status(HttpStatus.ACCEPTED)
+                .body(memberDto);
     }
 }

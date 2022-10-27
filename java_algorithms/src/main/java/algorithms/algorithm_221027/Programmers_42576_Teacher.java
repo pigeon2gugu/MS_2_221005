de9f423860e245adcf4421e424fadc7af59ae3e3
pigeon2gugu <khn1135@gmail.com>
2022-10-27 10:30:13 +0900

    20221026 algorithm Hash example Programmers 42576 teacher method 1. Resolve duplication problem

--- java_algorithms/src/main/java/algorithms/algorithm_221027/Programmers_42576_Teacher.java
+++ java_algorithms/src/main/java/algorithms/algorithm_221027/Programmers_42576_Teacher.java
@@ -6,23 +6,24 @@ import java.util.Map;
 //https://school.programmers.co.kr/learn/courses/30/lessons/42576?language=java
 //완주하지 못한 선수
 
-//동명이인 처리 전단계
+//동명이인 처리
 public class Programmers_42576_Teacher {
 
     public String solution(String[] participant, String[] completion) {
 
         HashMap<String, Integer> ptp = new HashMap<>();
 
-        //참가자 HashMap에 add. key : name, value : 1 로 초기화
+        //참가자 HashMap에 add. key : name, value : count. 동명이인의 경우 count로 더해줌
         for(String i : participant) {
-            if(ptp.get(i) == null)  {
-                ptp.put(i, 1);
+            if(!ptp.containsKey(i))  {
+                ptp.put(i, 0);
             }
+            ptp.put(i,ptp.get(i)+1);
         }
 
-        //완주자의 name이 HashMap에 key로 존재하면 0으로 check
+        //완주자의 name이 HashMap에 key로 존재하면 count -1 해준다.
         for(String i : completion) {
-            ptp.put(i, 0);
+            ptp.put(i, ptp.get(i)-1);
         }
 
         String answer = "";

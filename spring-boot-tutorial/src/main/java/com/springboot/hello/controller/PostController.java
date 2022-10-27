2014ad6058fbad46931174fd3caee00ff04deb82
pigeon2gugu <khn1135@gmail.com>
2022-10-27 14:16:54 +0900

    spring-boot tutorial 1. PostController

---
+++ spring-boot-tutorial/src/main/java/com/springboot/hello/controller/PostController.java
@@ -0,0 +1,34 @@
+package com.springboot.hello.controller;
+
+import com.springboot.hello.domain.dto.MemberDto;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+//Post로 API요청
+@RestController
+@RequestMapping("/api/v1/post-api")
+public class PostController {
+
+    @RequestMapping(value = "/domain", method = RequestMethod.POST)
+    public String postExample() {
+        return "Hello Post API";
+    }
+
+    
+    @PostMapping("/member")
+    //Post에서는 @RequestBody를 주로 사용.
+    //json(등)형식으로 넘어온 데이터를 사용할 수 있게 해줌.
+    //어떤 타입으로 데이터가 들어올지 Sever입장에서는 예측할 수 없기 때문에 Value를 최상위 class Object로 처리.
+    public String postMember(@RequestBody Map<String, Object> postData) {
+        StringBuilder sb = new StringBuilder();
+        postData.entrySet().forEach(map->sb.append(map.getKey()+":"+map.getValue()+"\n"));
+        return sb.toString();
+
+    }
+
+    @PostMapping("/member2")
+    public String postMemberDto(@RequestBody MemberDto memberDto) {
+        return memberDto.toString();
+    }
+}

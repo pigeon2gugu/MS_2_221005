e9afcb94fa5947904db615dbeacf379f5fc20102
pigeon2gugu <khn1135@gmail.com>
2022-10-28 11:17:06 +0900

    fix : fix error at 11 and 14 test case

--- java_algorithms/src/main/java/algorithms/algorithm_221028/Programmers_42577.java
+++ java_algorithms/src/main/java/algorithms/algorithm_221028/Programmers_42577.java
@@ -15,16 +15,19 @@ public class Programmers_42577 {
 
         HashSet<String> pb2 = new HashSet<>();
 
+        //전화번호의 최소길이로 탐색하는 것은 잘못된 풀이 방법이었다.
+        //일일히 탐색 필요.
+
         for(String key : pb.keySet()) {
-            pb2.add(key.substring(0,minKey));
+            for(int j =1; j <key.length(); j++) {
+                if(pb.containsKey(key.substring(0,j))) {
+                    return false;
+                }
+            }
         }
 
-        if(pb.size() != pb2.size()) {
-            return false;
-        }
-        else {
-            return true;
-        }
+        return true;
+
 
     }
 }

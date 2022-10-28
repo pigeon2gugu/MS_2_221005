22e0f22ddccce62df3f88c303c156b22663cc99e
pigeon2gugu <khn1135@gmail.com>
2022-10-28 11:35:18 +0900

    feat: add log info

--- spring-boot-tutorial/src/main/java/com/springboot/hello/controller/GetController.java
+++ spring-boot-tutorial/src/main/java/com/springboot/hello/controller/GetController.java
@@ -1,6 +1,7 @@
 package com.springboot.hello.controller;
 
 import com.springboot.hello.domain.dto.MemberDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
@@ -8,11 +9,13 @@ import java.util.Map;
 //dispatcherServlet이 매핑해줄 Controller를 등록
 @RestController
 @RequestMapping("/api/v1/get-api")
-public class HelloController {
+@Slf4j //log info를 위한 annotation
+public class GetController {
 
     //컨트롤러에 어떤 매소드를 할당할 지 정해줌.
     @RequestMapping(value = "/hello", method = RequestMethod.GET) //path
     public String hello() {
+        log.info("hello로 요청이 들어왔습니다.");
         return "Hello World";
     }
 
@@ -23,6 +26,7 @@ public class HelloController {
 
     @GetMapping(value = "/variable1/{variable}") //path
     public String getVariable1(@PathVariable String variable) {
+        log.info("getVariable1로 요청이 들어왔습니다. variable:{}", variable);
         return variable;
     }
 

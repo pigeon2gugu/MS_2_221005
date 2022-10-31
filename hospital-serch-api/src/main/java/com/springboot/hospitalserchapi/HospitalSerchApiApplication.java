9fb17de963138d1541481f93b69caa05b2c07c7e
pigeon2gugu <khn1135@gmail.com>
2022-10-31 14:22:30 +0900

    first commit

---
+++ hospital-serch-api/src/main/java/com/springboot/hospitalserchapi/HospitalSerchApiApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.hospitalserchapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalSerchApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitalSerchApiApplication.class, args);
+	}
+
+}

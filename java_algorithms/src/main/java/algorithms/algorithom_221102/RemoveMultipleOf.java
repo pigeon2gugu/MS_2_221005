72b23492947609c1abc0581fac77c3a9b925a793
pigeon2gugu <khn1135@gmail.com>
2022-11-02 10:34:12 +0900

    효율성 향상 시도 list -> array

--- java_algorithms/src/main/java/algorithms/algorithom_221102/RemoveMultipleOf.java
+++ java_algorithms/src/main/java/algorithms/algorithom_221102/RemoveMultipleOf.java
@@ -3,27 +3,40 @@ package algorithms.algorithom_221102;
 //에라토스테네스 채로 소수 구하기. step 1
 //50 까지의 소수 찾기
 //sqrt(50)이하의 수의 배수 모두 지우기
+//list로 할 시 효율성테스트 실패. (remove시 리스트 복사 문제)
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class RemoveMultipleOf {
     public int solution(int n) {
 
-        ArrayList<Integer> nums = new ArrayList<Integer>();
+        int answer = 0;
+
+        int [] nums = new int[n-1];
+        boolean [] numChecks = new boolean[n-1];
+
+
         for(int i = 2; i <= n; i++) {
-            nums.add(i);
+            nums[i-2] = i;
+            numChecks[i-2] = true;
         }
 
-        for(int i = 2; i <= Math.sqrt(n); i++) {
-            for(int j = 1; j < nums.size(); j ++) {
-                if(nums.get(j)%i==0 && nums.get(j) != i){
-                    nums.remove(nums.get(j));
-                    j--;
+        for(int i = 2; i*i <= n; i++) {
+            for(int j = 0; j < nums.length; j ++) {
+                if(nums[j]%i==0 && nums[j] != i){
+                    numChecks[j] = false;
                 }
             }
         }
-        System.out.println(nums);
-        return nums.size();
+
+        for(int i = 0; i < numChecks.length; i++) {
+            if(numChecks[i] == true){
+                answer += 1;
+            }
+        }
+
+        return answer;
     }
 
     public static void main(String[] args) {

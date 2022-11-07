2908661988a0bd74d573f116db9f0f52e892a0b8
pigeon2gugu <khn1135@gmail.com>
2022-11-07 09:33:37 +0900

    프로그래머스 하샤드 수 구하기 2단계 하샤드 수 검사

--- java_algorithms/src/main/java/algorithms/algorithm_221107/HarshadNumber.java
+++ java_algorithms/src/main/java/algorithms/algorithm_221107/HarshadNumber.java
@@ -4,20 +4,27 @@ public class HarshadNumber {
     public int sumOfDigits(int num) {
         int len = (int) (Math.log(num));
         int sum = 0;
-        int num1 = num;
 
         for(int i = len-1; i >= 0; i --) {
 
-            sum += (int) (num1/Math.pow(10, i));
-            num1 = (int) ((num1%Math.pow(10, i)));
+            sum += (int) (num/Math.pow(10, i));
+            num = (int) ((num%Math.pow(10, i)));
 
         }
 
         return sum;
     }
 
+    public boolean solution(int x) {
+        if(x%sumOfDigits(x) == 0) {
+            return true;
+        }
+        return false;
+    }
+
     public static void main(String[] args) {
         HarshadNumber hn = new HarshadNumber();
         System.out.println(hn.sumOfDigits(123));
+        System.out.println(hn.solution(18));
     }
 }

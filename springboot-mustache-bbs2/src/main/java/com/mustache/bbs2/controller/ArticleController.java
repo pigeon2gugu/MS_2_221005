6d6f83573240e97c35af1d1d23141e82aadffa8f
pigeon2gugu <khn1135@gmail.com>
2022-11-08 14:30:42 +0900

    id를 입력하여 show or error page 띄우기

--- springboot-mustache-bbs2/src/main/java/com/mustache/bbs2/controller/ArticleController.java
+++ springboot-mustache-bbs2/src/main/java/com/mustache/bbs2/controller/ArticleController.java
@@ -3,17 +3,24 @@ package com.mustache.bbs2.controller;
 import com.mustache.bbs2.domain.Article;
 import com.mustache.bbs2.domain.dto.ArticleDto;
 import com.mustache.bbs2.domain.repository.ArticleRepository;
+import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/articles")
 @Slf4j //log기능 추가
 public class ArticleController {
 
+    //Spring이 ArticleRepository구현체 (ArticleDao라고 보면 됨)를 DI (interface 아님)
+    //기능 : findAll(), findById(), save(), deleteAll() ... 등이 자동으로 만들어 짐.
     private final ArticleRepository articleRepository;
 
     public ArticleController(ArticleRepository articleRepository) {
@@ -26,6 +33,19 @@ public class ArticleController {
         return "articles/new"; //articles diractory의 new.mustache로
     }
 
+    @GetMapping("/{id}")
+    public String selectSingle(@PathVariable Long id, Model model) {
+        Optional<Article> optArticle = articleRepository.findById(id);
+
+        if (!optArticle.isEmpty()) {
+            //Optinal.get() -> Article
+            model.addAttribute("article", optArticle.get());
+            return "articles/show";
+        } else {
+            return "articles/error";
+        }
+
+    }
     @PostMapping("")
     public String add(ArticleDto articleDto) {
         log.info(articleDto.getTitle(), articleDto.getContent());

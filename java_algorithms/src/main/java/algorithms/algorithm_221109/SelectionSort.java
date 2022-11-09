66bc5e0bb7f54c187e66cbdc55e4c96a83b4fa9b
pigeon2gugu <khn1135@gmail.com>
2022-11-09 10:25:44 +0900

    selection sort 내림차순, 오름차순 interface로 구현

--- java_algorithms/src/main/java/algorithms/algorithm_221109/SelectionSort.java
+++ java_algorithms/src/main/java/algorithms/algorithm_221109/SelectionSort.java
@@ -2,15 +2,16 @@ package algorithms.algorithm_221109;
 
 import java.util.Arrays;
 
+interface StatementStrategy {
+    boolean apply(int a, int b);
+}
 public class SelectionSort {
 
-    public static void main(String[] args) {
-        int[] arr = new int[]{2, 7, 4, 9, 10, 223, 111, 23, 3, 39};
-
+    public int[] selectionSort(int[] arr, StatementStrategy stmt) {
         for(int i = 0; i < arr.length; i ++) {
             int minIdx = i;
             for(int j = i; j < arr.length; j++) {
-                if(arr[j] < arr[minIdx]) {
+                if(stmt.apply(arr[minIdx], arr[j])) {
                     minIdx = j;
                 }
             }
@@ -18,7 +19,17 @@ public class SelectionSort {
             arr[i] = arr[minIdx];
             arr[minIdx] = temp;
         }
-
         System.out.println(Arrays.toString(arr));
+        return arr;
+    }
+
+
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{2, 7, 4, 9, 10, 223, 111, 23, 3, 39};
+        SelectionSort ss = new SelectionSort();
+        ss.selectionSort(arr, (a,b) -> a< b); //내림차순
+        ss.selectionSort(arr, (a,b) -> a> b); //오름차순
+
     }
 }

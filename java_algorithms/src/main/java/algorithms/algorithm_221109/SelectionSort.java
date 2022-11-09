9b800ac3ea38cec5065190983386a65e14f1a6b2
pigeon2gugu <khn1135@gmail.com>
2022-11-09 10:45:00 +0900

    selection sort 내림차순, 오름차순 BiFuction사용 추가

--- java_algorithms/src/main/java/algorithms/algorithm_221109/SelectionSort.java
+++ java_algorithms/src/main/java/algorithms/algorithm_221109/SelectionSort.java
@@ -1,6 +1,8 @@
 package algorithms.algorithm_221109;
 
 import java.util.Arrays;
+import java.util.function.BiFunction;
+import java.util.function.Function;
 
 interface StatementStrategy {
     boolean apply(int a, int b);
@@ -23,13 +25,37 @@ public class SelectionSort {
         return arr;
     }
 
+    public int[] selectionSort2(int[] arr, BiFunction<Integer, Integer, Boolean> stmt) {
+        for(int i = 0; i < arr.length; i ++) {
+            int minIdx = i;
+            for(int j = i; j < arr.length; j++) {
+                if(stmt.apply(arr[minIdx], arr[j])) {
+                    minIdx = j;
+                }
+            }
+            int temp = arr[i];
+            arr[i] = arr[minIdx];
+            arr[minIdx] = temp;
+        }
+        System.out.println(Arrays.toString(arr));
+        return arr;
+    }
+
 
 
     public static void main(String[] args) {
         int[] arr = new int[]{2, 7, 4, 9, 10, 223, 111, 23, 3, 39};
         SelectionSort ss = new SelectionSort();
-        ss.selectionSort(arr, (a,b) -> a< b); //내림차순
-        ss.selectionSort(arr, (a,b) -> a> b); //오름차순
+        ss.selectionSort2(arr, (a,b) -> a< b); //내림차순
+        ss.selectionSort2(arr, (a,b) -> a> b); //오름차순
+
+        //Fuction<input type, return type>
+        //integer배열을 input으로 받아, boolean return.
+        Function<Integer[], Boolean> fn = arr1 -> arr1[0] > arr1[1];
+        System.out.println(fn.apply(new Integer[]{10, 20}));
+
+        BiFunction<Integer, Integer, Boolean> biFunction = (a, b) -> a > b;
+
 
     }
 }

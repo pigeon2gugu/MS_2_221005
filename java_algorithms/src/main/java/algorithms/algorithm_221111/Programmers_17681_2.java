b7c68669e4a4646b092377cc370182b2b675b8bf
pigeon2gugu <khn1135@gmail.com>
2022-11-11 10:41:46 +0900

    Programmers [1차] 비밀지도 비트연산 이용 solution

--- java_algorithms/src/main/java/algorithms/algorithm_221111/Programmers_17681_2.java
+++ java_algorithms/src/main/java/algorithms/algorithm_221111/Programmers_17681_2.java
@@ -8,28 +8,16 @@ import java.util.ArrayList;
 public class Programmers_17681_2 {
     class Solution {
 
-        String getBinaryString(int decNum) {
-            String answer = "";
-            while (decNum > 0) {
-                answer += decNum % 2;
-                decNum /= 2;
-            }
-            return answer;
-        }
 
         public ArrayList<String> solution(int n, int[] arr1, int[] arr2) {
 
-            ArrayList<String> binaryArr1 = new ArrayList<>();
-            ArrayList<String> binaryArr2 = new ArrayList<>();
             ArrayList<String> answer1 = new ArrayList<>();
 
             for(int i = 0; i< arr1.length; i++) {
-                binaryArr1.add("0".repeat(n- getBinaryString(arr1[i]).length()) + getBinaryString(arr1[i]));
-                binaryArr1.add("0".repeat(n- getBinaryString(arr2[i]).length()) + getBinaryString(arr2[i]));
+                String temp = Integer.toBinaryString(arr1[i] | arr2[i]).replace("1", "#").replace("0", " ");
+                answer1.add(" ".repeat(n - temp.length()) +temp );
             }
 
-            System.out.println(binaryArr1);
-            System.out.println(binaryArr2);
             return answer1;
         }
 

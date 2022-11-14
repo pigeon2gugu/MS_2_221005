02efe3e817ae2a1d329fbc80af9c68515b8465ca
pigeon2gugu <khn1135@gmail.com>
2022-11-14 15:19:49 +0900

    병상 수 검색 method, test code 추가

--- springboot-mustache-bbs4/src/main/java/com/mustache/bbs4/domain/repository/HospitalRepository.java
+++ springboot-mustache-bbs4/src/main/java/com/mustache/bbs4/domain/repository/HospitalRepository.java
@@ -7,5 +7,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface HospitalRepository extends JpaRepository<Hospital, Integer> {
-    List<Hospital> findByBusinessTypeNameInAndRoadNameAddressContaining(List<String> businessTypes, String address);
+    List<Hospital> findByBusinessTypeNameIn(List<String> businessTypes);
+
+    List<Hospital> findByRoadNameAddressContaining(String keyword); // 포함
+    List<Hospital> findByHospitalNameStartsWith(String keyword); // 시작
+    List<Hospital> findByHospitalNameEndsWith(String keyword); // 끝남
+
+    List<Hospital> findByPatientRoomCountBetween(int a, int b);
+    
+    //내림차순까지 포함
+    List<Hospital> findByPatientRoomCountBetweenOrderByPatientRoomCountDesc(int a, int b);
+
 }
--- springboot-mustache-bbs4/src/test/java/com/mustache/bbs4/domain/repository/HospitalRepositoryTest.java
+++ springboot-mustache-bbs4/src/test/java/com/mustache/bbs4/domain/repository/HospitalRepositoryTest.java
@@ -26,11 +26,41 @@ class HospitalRepositoryTest {
         inClues.add("보건진료소");
 
         String address = "관악구";
-        List<Hospital> hospitals = hospitalRepository.findByBusinessTypeNameInAndRoadNameAddressContaining(inClues, address);
+        List<Hospital> hospitals = hospitalRepository.findByBusinessTypeNameIn(inClues);
 
-        for(var hospital : hospitals) {
-            System.out.println(hospital.getHospitalName());
+        printHospitalNameAndAddress(hospitals);
+    }
+
+    @Test
+    void containing() {
+        List<Hospital> hospitals = hospitalRepository.findByRoadNameAddressContaining("관악구");
+        printHospitalNameAndAddress(hospitals);
+    }
+
+    @Test
+    void startsWith() {
+        List<Hospital> hospitals = hospitalRepository.findByHospitalNameStartsWith("서울");
+        printHospitalNameAndAddress(hospitals);
+    }
+
+    @Test
+    void endswith() {
+        List<Hospital> hospitals = hospitalRepository.findByHospitalNameEndsWith("병원"); //의원, 병원, 이비인후과, 치과 ...
+        printHospitalNameAndAddress(hospitals);
+    }
+
+    @Test
+    void findByPatientRoomCountAndPatientRoomCount() {
+        List<Hospital> hospitals = hospitalRepository.findByPatientRoomCountBetweenOrderByPatientRoomCountDesc(10, 20);// 의원, 병원, 이비인후과, 치과
+        printHospitalNameAndAddress(hospitals);
+    }
+
+    void printHospitalNameAndAddress(List<Hospital> hospitals) {
+        for (var hospital : hospitals) {
+            System.out.printf("%s | %s %f\n", hospital.getHospitalName(), hospital.getRoadNameAddress(), hospital.getTotalAreaSize());
         }
+
+        System.out.println(hospitals.size());
     }
 
 }

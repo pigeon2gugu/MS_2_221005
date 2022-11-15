50b5ce397c4d5b152f43d7d286d13a235245580c
pigeon2gugu <khn1135@gmail.com>
2022-11-15 10:50:00 +0900

    quicksort solution 재귀 이용

--- java_algorithms/src/main/java/algorithms/algorithm_221115/QuickSort.java
+++ java_algorithms/src/main/java/algorithms/algorithm_221115/QuickSort.java
@@ -6,12 +6,20 @@ import java.util.List;
 //재귀 이용
 public class QuickSort {
 
-    public static void main(String[] args) {
+    public List<Integer> merge(List<Integer> left, List<Integer> middle, List<Integer> right) {
+        List<Integer> answer = new ArrayList<>();
+        answer.addAll(left);
+        answer.addAll(middle);
+        answer.addAll(right);
+        return answer;
+    }
 
-        int[] arr = new int[] {20, 18, 5, 19, 5, 25, 40, 50};
+    public List<Integer> sort(List<Integer> arr) {
 
-        int idx = (int)(arr.length/2);
-        int pivot = arr[idx];
+        if(arr.size() <= 1) return arr;
+
+        int idx = (int)(arr.size()/2);
+        int pivot = arr.get(idx);
 
         List<Integer> left = new ArrayList<>();
         List<Integer> right = new ArrayList<>();
@@ -19,20 +27,35 @@ public class QuickSort {
 
         middle.add(pivot);
 
-        for(int i = 0; i<arr.length; i++) {
+        for(int i = 0; i<arr.size(); i++) {
             if(i == idx) {
                 continue;
             }
-            if(arr[i] <= pivot) {
-                left.add(arr[i]);
+            if(arr.get(i) <= pivot) {
+                left.add(arr.get(i));
             } else {
-                right.add(arr[i]);
+                right.add(arr.get(i));
             }
         }
 
         System.out.println(left);
-        System.out.println(middle);
         System.out.println(right);
+
+        return merge(sort(left), middle, sort(right));
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = new int[] {20, 18, 5, 19, 5, 25, 40, 50};
+        List<Integer> al = new ArrayList<>();
+        for(int i = 0; i<arr.length; i++) {
+            al.add(arr[i]);
+        }
+
+        QuickSort qs = new QuickSort();
+        System.out.println(qs.sort(al));
+
+
     }
 
 }

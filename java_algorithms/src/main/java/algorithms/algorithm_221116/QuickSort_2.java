0db099ed8f6c0df44d75d88ebcb5c6da3744a782
pigeon2gugu <khn1135@gmail.com>
2022-11-16 09:40:12 +0900

    quicksort solution array이용 1단계 1. pivot을 정한다 2. pivot보다 arr[leftIdx]가 작으면 leftIdx +=1 3. pivot보다 arr[rightIdx]가 크면 rightIdx -=1 4. leftIdx <= rightIdx 면 교환 5. 교환 했으면 leftIdx+=1, rightIdx -= 1

---
+++ java_algorithms/src/main/java/algorithms/algorithm_221116/QuickSort_2.java
@@ -0,0 +1,42 @@
+package algorithms.algorithm_221116;
+
+import java.util.Arrays;
+
+//array이용
+//이전 로직은 공간 복잡도가 높음
+//GarbageCollection문제 → List를 만들고 merge. GC문제가 생길 수 있음.
+public class QuickSort_2 {
+
+    public static void main(String[] args) {
+        int[] arr = new int[] {20, 18, 5, 19, 40, 50, 5, 25 };
+        int pivot = arr[arr.length/2];
+        int leftIdx = 0;
+        int rightIdx = arr.length-1;
+
+        while(true) {
+            while (arr[leftIdx] < pivot) {
+                leftIdx += 1;
+            }
+
+            while (arr[rightIdx] > pivot) {
+                rightIdx -= 1;
+            }
+
+            if (leftIdx <= rightIdx) {
+                int temp = arr[leftIdx];
+                arr[leftIdx] = arr[rightIdx];
+                arr[rightIdx] = temp;
+                leftIdx += 1;
+                rightIdx -= 1;
+            } else {
+                break;
+            }
+        }
+
+
+
+        System.out.println(Arrays.toString(arr));
+    }
+
+
+}

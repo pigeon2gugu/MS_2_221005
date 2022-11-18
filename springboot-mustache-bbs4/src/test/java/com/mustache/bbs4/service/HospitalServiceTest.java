ab75b2c7b8a2a85e6165f3d6a5be60db58526e2c
pigeon2gugu <khn1135@gmail.com>
2022-11-18 11:52:09 +0900

    영업 코드 test code (영업중, 폐업 여부 검사)

--- springboot-mustache-bbs4/src/test/java/com/mustache/bbs4/service/HospitalServiceTest.java
+++ springboot-mustache-bbs4/src/test/java/com/mustache/bbs4/service/HospitalServiceTest.java
@@ -31,14 +31,27 @@ class HospitalServiceTest {
             "patientRoomCount":0,"totalNumberOfBeds":0,"businessTypeName":"치과의원",
             "businessStatusName":"영업중","totalAreaSize":52.29}*/
 
+/*    {"id":74581,"roadNameAddress":"서울특별시 중구 퇴계로 127 (충무로2가,2층)","hospitalName":"아이리스치과의원",
+            "patientRoomCount":0,"totalNumberOfBeds":0,"businessTypeName":"치과의원",
+            "businessStatusName":"폐업","totalAreaSize":195.0}*/
+
     void statusTest() {
         Mockito.when(hospitalRepository.findById(1))
                 .thenReturn(Optional.of(new Hospital(1, "광주광역시 북구 동문대로 24, 3층 (풍향동)", "효치과의원",
                         0, 0, "치과의원",
                         13, (float) 52.29)));
 
+        Mockito.when(hospitalRepository.findById(74581))
+                .thenReturn(Optional.of(new Hospital(74581, "서울특별시 중구 퇴계로 127 (충무로2가,2층)", "아이리스치과의원",
+                        0, 0, "치과의원",
+                        3, (float) 195.0)));
+
+
         HospitalResponse hospitalResponse = hospitalService.getHospital(1);
+        HospitalResponse hospitalResponse2 = hospitalService.getHospital(74581);
+
         assertEquals("영업중", hospitalResponse.getBusinessStatusName());
+        assertEquals("폐업", hospitalResponse2.getBusinessStatusName());
     }
 
 

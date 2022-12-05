534056538efeacea0111c60d1953c44e1ae460db
pigeon2gugu <khn1135@gmail.com>
2022-12-05 09:45:19 +0900

    HeapSort 1단계 항상 부모가 크도록 정렬

--- java_algorithms/src/main/java/algorithms/algorithm_221205/HeapSort.java
+++ java_algorithms/src/main/java/algorithms/algorithm_221205/HeapSort.java
@@ -13,6 +13,7 @@ public class HeapSort {
     public static int[] heap(int[] arr) {
 
         int idx = arr.length -1;
+        int cnt = 0;
 
         while(idx > 0) {
 
@@ -20,18 +21,21 @@ public class HeapSort {
                int temp = arr[(int) Math.floor( (double) (idx -1)/2)];
                arr[(int) Math.floor( (double) (idx -1)/2)] = arr[idx];
                arr[idx] = temp;
+               cnt += 1;
             }
 
             idx -= 1;
         }
 
-        System.out.println(Arrays.toString(arr));
+        if (cnt > 0) {
+            return heap(arr);
+        }
 
         return arr;
     }
 
     public static void main(String[] args) {
-        int [] arr = {6, 7, 5, 8};
-        heap(arr);
+        int [] arr = {6, 7, 5, 8 ,9};
+        System.out.println(Arrays.toString(heap(arr)));
     }
 }

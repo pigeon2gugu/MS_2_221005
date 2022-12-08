2213ba9bdb73915e098e39c47a7c7f63ca3fdaca
pigeon2gugu <khn1135@gmail.com>
2022-12-08 09:41:27 +0900

    피보나치 수열 2가지 방법 구현

--- java_algorithms/src/main/java/algorithms/algorithm_221208/FibonacciSequence.java
+++ java_algorithms/src/main/java/algorithms/algorithm_221208/FibonacciSequence.java
@@ -15,7 +15,22 @@ public class FibonacciSequence {
         return memo[num];
     }
 
+    public static long fib2(int n, long[] memo) {
+        if(n == 0) throw new IllegalArgumentException("n은 1이상 이어야 한다.");
+        if (n <=2 ) return 1;
+
+        if(memo[n] == 0){
+            memo[n] = fib2(n - 1, memo) + fib2(n - 2, memo);
+        }
+        return memo[n];
+    }
+
+
     public static void main(String[] args) {
-        System.out.println(fib(40));
+        System.out.println(fib(5));
+
+        int n = 5;
+        var r = (fib2(n, new long[n + 1]));
+        System.out.println(r);
     }
 }
